0ec6f28c6e0ec0b4f73aadef5db96ec14cdaab9c
ADMINISTRADOR 1 <jf.castillog@uea.edu.ec>
2024-07-19 20:28:32 -0500

    Dashboard.py

--- Dashboard.py
+++ Dashboard.py
@@ -1,21 +1,25 @@
-import os
+import os # Importa el módulo os para interactuar con el sistema operativo
 
 def mostrar_codigo(ruta_script):
     # Asegúrate de que la ruta al script es absoluta
     ruta_script_absoluta = os.path.abspath(ruta_script)
     try:
+        # Verifica si el archivo existe en la ruta absoluta
         with open(ruta_script_absoluta, 'r') as archivo:
             print(f"\n--- Código de {ruta_script} ---\n")
+            # Imprime el contenido del archivo
             print(archivo.read())
     except FileNotFoundError:
+        # Mensaje si el archivo no se encuentra
         print("El archivo no se encontró.")
     except Exception as e:
+        # Mensaje para cualquier otro error
         print(f"Ocurrió un error al leer el archivo: {e}")
 
 def mostrar_menu():
     # Define la ruta base donde se encuentra el dashboard.py
     ruta_base = os.path.dirname(__file__)
-
+    # Diccionario con las opciones del menú y las rutas relativas de los scripts
     opciones = {
         '1': 'Unidad 1/Semana 2/EJEMPLO DE ABSTRACCION.py',
         '2': 'Unidad 1/Semana 2/EJEMPLO DE ENCAPSULACION.py',
@@ -28,7 +32,7 @@ def mostrar_menu():
         '9': 'Unidad 2/Semana 7/Constructores y Destructores.py',
         # Agrega aquí el resto de las rutas de los scripts
     }
-
+    # Bucle infinito para mostrar el menú hasta que el usuario elija salir
     while True:
         print("\nMenu Principal - Dashboard")
         # Imprime las opciones del menú
@@ -47,6 +51,6 @@ def mostrar_menu():
         else:
             print("Opción no válida. Por favor, intenta de nuevo.")
 
-# Ejecutar el dashboard
+# Ejecutar el menú principal si este script se ejecuta directamente
 if __name__ == "__main__":
     mostrar_menu()
